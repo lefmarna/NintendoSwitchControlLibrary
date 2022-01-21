4fc22a8889b92a0ca6773d25566847313532394d
lefmarna <panume5ya@gmail.com>
2022-01-21 23:57:20 +0900

    コメントの書き方を修正

--- src/NintendoSwitchControlLibrary.cpp
+++ src/NintendoSwitchControlLibrary.cpp
@@ -8,19 +8,18 @@ https://opensource.org/licenses/mit-license.php
 
 // ボタンを押してから離すまでの時間（ミリ秒）
 const uint16_t INPUT_TIME = 100;
+
 // ボタンを連打する際に、押して認識後、離して認識するまでの合計時間（ミリ秒）
 const uint16_t INPUT_TIME_TWICE = INPUT_TIME * 2;
 
+/**
+ * Switchコントローラーのボタンを押す
+ *
+ * @param uint16_t button     押すボタン
+ * @param int      delay_time ボタンを押した後の待ち時間（1秒 = 1000）
+ * @param int      loop       ボタンを押す回数（省略可、デフォルトは1）
+ */
 void pushButton(uint16_t button, int delay_time, int loop) {
-    /*
-    Switchコントローラーのボタンを押す関数
-
-    Parameters
-    -------------------
-        button: 押すボタン
-        delay_time: ボタンを押した後の待ち時間（1秒 = 1000）
-        loop: ボタンを押す回数（省略可、デフォルトは1）
-    */
     for (int i = 0; i < loop; i++) {
         SwitchControlLibrary().pressButton(button);
         SwitchControlLibrary().sendReport();
@@ -32,15 +31,13 @@ void pushButton(uint16_t button, int delay_time, int loop) {
     delay(INPUT_TIME);
 }
 
+/**
+ * Switchコントローラーのボタンを指定の時間押し続ける
+ *
+ * @param uint16_t button    押し続けるボタン
+ * @param int      hold_time ボタンを押す時間の長さ（1秒 = 1000）
+ */
 void holdButton(uint16_t button, int hold_time) {
-    /*
-    Switchコントローラーのボタンを指定の時間押し続ける関数
-
-    Parameters
-    -------------------
-        button: 押し続けるボタン
-        hold_time: ボタンを押す時間の長さ（1秒 = 1000）
-    */
     SwitchControlLibrary().pressButton(button);
     SwitchControlLibrary().sendReport();
     delay(hold_time);
@@ -49,16 +46,14 @@ void holdButton(uint16_t button, int hold_time) {
     delay(INPUT_TIME);
 }
 
+/**
+ * Switchコントローラーの十字キーを押す
+ *
+ * @param uint8_t hat        押す十字キーのボタン
+ * @param int     delay_time ボタンを押した後の待ち時間（1秒 = 1000）
+ * @param int     loop       ボタンを押す回数（省略可、デフォルトは1）
+ */
 void pushHat(uint8_t hat, int delay_time, int loop) {
-    /*
-    Switchコントローラーの十字キーを押す関数
-
-    Parameters
-    -------------------
-        hat: 押す十字キーのボタン
-        delay_time: ボタンを押した後の待ち時間（1秒 = 1000）
-        loop: ボタンを押す回数（省略可、デフォルトは1）
-    */
     for (int i = 0; i < loop; i++) {
         SwitchControlLibrary().pressHatButton(hat);
         SwitchControlLibrary().sendReport();
@@ -70,15 +65,13 @@ void pushHat(uint8_t hat, int delay_time, int loop) {
     delay(INPUT_TIME);
 }
 
+/**
+ * Switchコントローラーの十字キーを指定の時間押し続ける
+ *
+ * @param uint8_t hat:       押し続ける十字キーのボタン
+ * @param int     hold_time: ボタンを押す時間の長さ（1秒 = 1000）
+ */
 void holdHat(uint8_t hat, int hold_time) {
-    /*
-    Switchコントローラーの十字キーを指定の時間押し続ける関数
-
-    Parameters
-    -------------------
-        hat: 押し続ける十字キーのボタン
-        hold_time: ボタンを押す時間の長さ（1秒 = 1000）
-    */
     SwitchControlLibrary().pressHatButton(hat);
     SwitchControlLibrary().sendReport();
     delay(hold_time);
@@ -87,26 +80,20 @@ void holdHat(uint8_t hat, int hold_time) {
     delay(INPUT_TIME);
 }
 
+/**
+ * Switchコントローラーの左スティックを指定の時間傾け続ける
+ * 128を基準とし、0~255の値を指定する
+ *
+ * @param uint8_t  lx:        左スティックのx軸
+ * @param uint8_t  ly:        左スティックのy軸
+ * @param int      tilt_time: スティックを傾ける時間の長さ
+ * @param uint16_t button:    連打するボタン
+ *
+ * @see Stick::MIN       0
+ * @see Stick::NEUTRAL 128
+ * @see Stick::MAX     255
+ */
 void tiltLeftStick(uint8_t lx, uint8_t ly, int tilt_time, uint16_t button = NULL) {
-    /*
-    Switchコントローラーの左スティックを指定の時間傾け続ける関数
-    128を基準とし、0~255の値を指定する
-
-    ボタンを指定すると、そのボタンを連打しながらスティックを倒し続ける動作となる
-
-    Parameters
-    -------------------
-        lx: 左スティックのx軸
-        ly: 左スティックのy軸
-        tilt_time: スティックを傾ける時間の長さ
-        button: 連打するボタン
-
-    Options
-    -------------------
-          0: Stick::MIN
-        128: Stick::NEUTRAL
-        255: Stick::MAX
-    */
     SwitchControlLibrary().moveLeftStick(lx, ly);
     SwitchControlLibrary().sendReport();
     if (button) {
@@ -126,26 +113,20 @@ void tiltLeftStick(uint8_t lx, uint8_t ly, int tilt_time, uint16_t button = NULL
     delay(INPUT_TIME);
 }
 
+/**
+ * Switchコントローラーの右スティックを指定の時間傾け続ける
+ * 128を基準とし、0~255の値を指定する
+ *
+ * @param uint8_t  rx:        右スティックのx軸
+ * @param uint8_t  ry:        右スティックのy軸
+ * @param int      tilt_time: スティックを傾ける時間の長さ
+ * @param uint16_t button:    連打するボタン
+ *
+ * @see Stick::MIN       0
+ * @see Stick::NEUTRAL 128
+ * @see Stick::MAX     255
+ */
 void tiltRightStick(uint8_t rx, uint8_t ry, int tilt_time, uint16_t button = NULL) {
-    /*
-    Switchコントローラーの右スティックを指定の時間傾け続ける関数
-    128を基準とし、0~255の値を指定する
-
-    ボタンを指定すると、そのボタンを連打しながらスティックを倒し続ける動作となる
-
-    Parameters
-    -------------------
-        rx: 右スティックのx軸
-        ry: 右スティックのy軸
-        tilt_time: スティックを傾ける時間の長さ
-        button: 連打するボタン
-
-    Options
-    -------------------
-          0: Stick::MIN
-        128: Stick::NEUTRAL
-        255: Stick::MAX
-    */
     SwitchControlLibrary().moveRightStick(rx, ry);
     SwitchControlLibrary().sendReport();
     if (button) {
@@ -165,29 +146,23 @@ void tiltRightStick(uint8_t rx, uint8_t ry, int tilt_time, uint16_t button = NUL
     delay(INPUT_TIME);
 }
 
+/**
+ * Switchコントローラーの左スティックと右スティックを同時に指定の時間傾け続ける
+ * 128を基準とし、0~255の値を指定する
+ *
+ * @param uint8_t  lx:        左スティックのx軸
+ * @param uint8_t  ly:        左スティックのy軸
+ * @param uint8_t  rx:        右スティックのx軸
+ * @param uint8_t  ry:        右スティックのy軸
+ * @param int      tilt_time: スティックを傾ける時間の長さ
+ * @param uint16_t button:    連打するボタン
+ *
+ * @see Stick::MIN       0
+ * @see Stick::NEUTRAL 128
+ * @see Stick::MAX     255
+ */
 void tiltLeftAndRightStick(uint8_t lx, uint8_t ly, uint8_t rx, uint8_t ry,
                            int tilt_time, uint16_t button = NULL) {
-    /*
-    Switchコントローラーの左スティックと右スティックを同時に指定の時間傾け続ける関数
-    128を基準とし、0~255の値を指定する
-
-    ボタンを指定すると、そのボタンを連打しながらスティックを倒し続ける動作となる
-
-    Parameters
-    -------------------
-        lx: 左スティックのx軸
-        ly: 左スティックのy軸
-        rx: 右スティックのx軸
-        ry: 右スティックのy軸
-        tilt_time: スティックを傾ける時間の長さ
-        button: 連打するボタン
-
-    Options
-    -------------------
-          0: Stick::MIN
-        128: Stick::NEUTRAL
-        255: Stick::MAX
-    */
     SwitchControlLibrary().moveLeftStick(lx, ly);
     SwitchControlLibrary().moveRightStick(rx, ry);
     SwitchControlLibrary().sendReport();
--- src/NintendoSwitchControlLibrary.h
+++ src/NintendoSwitchControlLibrary.h
@@ -11,9 +11,11 @@ https://opensource.org/licenses/mit-license.php
 // 各種ボタンの設定(ABXYだけでなく、HOMEやCAPTUREまで幅広く対応)
 void pushButton(uint16_t button, int delay_time = 0, int loop = 1);
 void holdButton(uint16_t button, int hold_time);
+
 // 十字キーの設定
 void pushHat(uint8_t hat, int delay_time = 0, int loop = 1);
 void holdHat(uint8_t hat, int hold_time);
+
 // スティックの設定
 void tiltLeftStick(uint8_t lx, uint8_t ly, int tilt_time, uint16_t button = NULL);
 void tiltRightStick(uint8_t rx, uint8_t ry, int tilt_time, uint16_t button = NULL);
